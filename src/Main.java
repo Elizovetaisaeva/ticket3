d2d491c275188b4cd6eaa5dcacc6c293658f4af9
Лаисат <lz_256@mail.ru>
2023-11-23 10:37:01 +0300

    ticket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String str = new String("");
+       int exactlyHalf = str.length() /2;
+       String firsPart = str.substring(0, exactlyHalf);
+       String secondPart = str.substring(exactlyHalf);
+        System.out.println(" Первая часть " + firsPart);
+        System.out.println(" Вторая часть " + secondPart);
+
     }
+
 }
